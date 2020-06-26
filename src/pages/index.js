2ba7b9cb462cb8a39288da91e66d030314090496
colsoncrim <colsonscrim@gmail.com>
2020-06-26 16:14:52 -0700

    Added more svg's and metrics section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,13 @@ import React from "react"
 import Layout from '../components/layout/Layout';
 import Button from '../components/Button';
 import Card from '../components/Card';
+import Metrics from '../components/Metrics';
 import HeroImage from "../svg/HeroImage";
+import Dropdown from "../svg/Dropdown";
+import AssetSelection from "../svg/AssetSelection";
+import '../styles/styles.css';
 
-
+       
 const IndexPage = () => (
   <Layout>
     <section className="pt-20 md:pt-40">
@@ -29,42 +33,7 @@ const IndexPage = () => (
       </div>
     </section>
 
-    <section className="py-20 lg:pb-40 lg:pt-48">
-      <div className="container mx-auto text-center">
-        <h2 className="font-bold text-2xl lg:text-5xl">Core Features</h2>
-        <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
-          <div className="flex-1 px-3">
-              <Card className="mb-8">
-                <p className="font-semibold text-xl">Feature One</p>
-                <p className="mt-4">
-                  An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                  velna vitae auctor integer.
-                </p>
-              </Card>
-          </div>
-          <div className="flex-1 px-3">
-              <Card className="mb-8">
-                <p className="font-semibold text-xl">Feature Two</p>
-                <p className="mt-4">
-                  An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                  velna vitae auctor integer.
-                </p>
-              </Card>
-          </div>
-          <div className="flex-1 px-3">
-              <Card className="mb-8">
-                <p className="font-semibold text-xl">Feature Three</p>
-                <p className="mt-4">
-                  An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                  velna vitae auctor integer.
-                </p>
-              </Card>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section className="py-8">
+    <section className="py-8 pt-40">
       <div className="container mx-auto px-2 pt-4 pb-12 text-gray-800">
         <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Pricing</h2>
         <div className="w-full mb-4">
@@ -125,6 +94,92 @@ const IndexPage = () => (
             </div>
           </div>
 
+        </div>
+      </div>
+    </section>
+
+    <section id="metrics-banner" className="py-20 lg:pt-30 bg-gray-300">
+      <div className="container mx-auto text-center">
+        <h3 className="text-white">Insert impressive stats here</h3>
+        <div className="flex flex-col sm:flex-row mt-8 lg:px-24 text-white">
+          <div className="w-full sm:w-1/3">
+            <Metrics primaryText="+90%" secondaryText="Impressive stats" />
+          </div>
+          <div className="w-full sm:w-1/3">
+            <Metrics primaryText="5x" secondaryText="Impressive stats" />
+          </div>
+          <div className="w-full sm:w-1/3">
+            <Metrics primaryText="+60%" secondaryText="Impressive stats" />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section className="py-20 lg:pb-30 lg:pt-30">
+      <div className="container mx-auto text-center">
+        <h2 className="font-bold text-2xl lg:text-5xl">Core Features</h2>
+        <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
+          <div className="flex-1 px-3">
+              <Card className="mb-8">
+                <p className="font-semibold text-xl">Feature One</p>
+                <p className="mt-4">
+                  An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
+                  velna vitae auctor integer.
+                </p>
+              </Card>
+          </div>
+          <div className="flex-1 px-3">
+              <Card className="mb-8">
+                <p className="font-semibold text-xl">Feature Two</p>
+                <p className="mt-4">
+                  An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
+                  velna vitae auctor integer.
+                </p>
+              </Card>
+          </div>
+          <div className="flex-1 px-3">
+              <Card className="mb-8">
+                <p className="font-semibold text-xl">Feature Three</p>
+                <p className="mt-4">
+                  An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
+                  velna vitae auctor integer.
+                </p>
+              </Card>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section className="pt-20 md:pt-40">
+      <div className="container mx-auto px-8 lg:flex">
+        <div className="text-center content-center lg:text-left lg:w-1/2">
+          <h3 className="text-2xl lg:text-3xl xl:text-4xl font-bold leading-none">
+            Highlight the benefits of using your product
+          </h3>
+          <p className="text-xl lg:text-2xl mt-4 font-thin">
+          An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
+          velna vitae auctor integer. 
+          </p>
+        </div>
+        <div className="lg:w-1/2 container mx-auto xl:flex">
+          <Dropdown />
+        </div>
+      </div>
+    </section>
+
+    <section className="pt-20 md:pt-40 pb-20">
+    <div className="container mx-auto px-8 pt-15 lg:flex">
+        <div className="lg:w-1/2 container mx-auto xl:flex">
+          <AssetSelection />
+        </div>
+        <div className="text-center content-center lg:text-left lg:w-1/2">
+          <h3 className="text-2xl lg:text-3xl xl:text-4xl font-bold leading-none">
+            Highlight the benefits of using your product
+          </h3>
+          <p className="text-xl lg:text-2xl mt-4 font-thin">
+          An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
+          velna vitae auctor integer. 
+          </p>
         </div>
       </div>
     </section>
